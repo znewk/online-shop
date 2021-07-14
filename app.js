b426e5e9d7757485a32631d307e9fc3c56597a1e
znewk <zane.css34@gmail.com>
2021-07-14 20:47:33 +0600

    first commi

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const TelegramBot = require("node-telegram-bot-api");
+const bodyParser = require('body-parser')
+
+const token = '1897548081:AAF2kHQiOXVhJqZk_LV9CjQbDEPokjVt0H8';
+
+const bot = new TelegramBot(token, { polling: true });
+const app = express()
+
+app.set("view engine", "hbs");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+
+// telegram bot <<
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'started')
+});
+
+bot.on("message", function(msg) {
+        bot.sendMessage(msg.chat.id, 'Hello')
+    })
+    // telegram bot >>
+
+app.listen(4040)
